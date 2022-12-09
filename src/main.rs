907cdbe9804a7e1b57c5a7a14e376b3b2c3bfbd4
WDominic <dom0_o@protonmail.com>
2022-12-08 23:46:54 -0500

    lowmaf actix-web server is live! (but does nothing)

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,35 @@ mod models;
 mod lowmaf;
 
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("tip-in-enrichment actix-web server is live!")
+    HttpResponse::Ok().body("lowmaf actix-web server is live!")
 }
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let cors = Cors::default()
+              .allow_any_origin()
+              .allow_any_header()
+              .allow_any_method();
+        let json_config = web::JsonConfig::default()
+            //.limit(4096)
+            .error_handler(|err, _req| {
+                // create custom error response
+                error::InternalError::from_response(err, HttpResponse::Conflict().finish())
+                    .into()
+            });
+        App::new()
+            .wrap(cors)
+            .service(
+                web::resource("/api/analyze/0/")
+                    .app_data(json_config)
+                    //.route(web::post().to(receive_data)),
+            ).service(
+                web::resource("/")
+                    .route(web::get().to(index)),
+            )
+    })
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
 }
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize}
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug)]
 pub struct LowmafInput {
