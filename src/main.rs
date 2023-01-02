4e0f5c9e6366f7c8ca40129bc4cab0165e0f4ffc
WDominic <dom0_o@protonmail.com>
2023-01-02 18:06:57 -0500

    restructured main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use actix_cors::Cors;
 use actix_web::{error, web, App, HttpResponse, HttpServer, Responder};
 use serde_json::Result;
 
@@ -9,32 +8,21 @@ async fn index() -> impl Responder {
     HttpResponse::Ok().body("lowmaf actix-web server is live!")
 }
 
+async fn analyze() -> impl Responder {
+    HttpResponse::Ok().body("Analyze function is working")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let cors = Cors::default()
-              .allow_any_origin()
-              .allow_any_header()
-              .allow_any_method();
-        let json_config = web::JsonConfig::default()
-            //.limit(4096)
-            .error_handler(|err, _req| {
-                // create custom error response
-                error::InternalError::from_response(err, HttpResponse::Conflict().finish())
-                    .into()
-            });
         App::new()
-            .wrap(cors)
             .service(
-                web::resource("/api/analyze/0/")
-                    .app_data(json_config)
-                    //.route(web::post().to(receive_data)),
-            ).service(
-                web::resource("/")
-                    .route(web::get().to(index)),
+                web::scope("/api")
+                    .route("/analyze/0/", web::get().to(analyze))
             )
+                // using scope allows us to more easily specify extra routes in the future
     })
-    .bind(("0.0.0.0", 8000))?
+    .bind(("127.0.0.1", 8000))?
     .run()
     .await
 }
