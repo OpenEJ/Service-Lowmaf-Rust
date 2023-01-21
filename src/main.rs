526fccbfc28cc1e7b60b1decec7d4ecaeb44e24c
W-Dominic <dom0_o@protonmail.com>
2023-01-21 09:07:03 -0500

    added index page, and allowed any origin policy

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use serde_json::Result;
 mod models;
 mod lowmaf;
 
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("lowmaf actix-web server is live!")
+}
+
 // extract json body and deserialize into LowMafInput struct
 // send Vec<LowMafInput> to begin() function located in lowmaf.rs
 async fn analyze(input: web::Json<Vec<models::LowmafInput>>) -> Result<impl Responder> {
@@ -17,7 +21,7 @@ async fn analyze(input: web::Json<Vec<models::LowmafInput>>) -> Result<impl Resp
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         let cors = Cors::default()
-              // .allow_any_origin()
+              .allow_any_origin()
               .allow_any_header()
               .allow_any_method();
         let json_config = web::JsonConfig::default()
@@ -31,6 +35,9 @@ async fn main() -> std::io::Result<()> {
             web::resource("/api/analyze/0/")
                 .app_data(json_config)
                 .route(web::post().to(analyze)),
+        ).service(
+            web::resource("/")
+                .route(web::get().to(index))
         )
     })
     .bind(("0.0.0.0", 8000))?
